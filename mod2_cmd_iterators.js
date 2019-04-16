8728083d788a2bd0bf706c9729090400b0eda3c1
JMSistemasInformaticos <jmoyadelgado@hotmail.com>
2019-04-16 17:39:34 -0500

    Update mod2_cmd_iterators.js

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,1 +1,26 @@
-// Type your code here
+// ejercicio p2p modulo 2
+// 1. 
+console.log(" ");
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log();
+// 2. 
+
+//let a = ["uno","-r","dos","uno","dos","tres","dos","tres","tres"];
+a= process.argv.slice(2,process.argv.length);
+console.log(a.sort());
+let arr = a.filter(a => a !== "-r");
+console.log(arr);
+let conta=0;
+anterior= arr[0];
+arr.forEach((val, index) => {
+  if (val !== anterior)
+   {
+  	console.log(`${anterior}: ${conta}`);
+	conta=0;
+	anterior=val;
+	}
+	conta+=1;
+})
+  	console.log(`${anterior}: ${conta}`);
+// 3. 
